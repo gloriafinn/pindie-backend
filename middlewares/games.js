856101b3d1ef9520cdc86c2807e81c28f43d8035
smiledariya <smiledariya@mail.ru>
2024-05-17 13:38:38 +0300

    Connect to db

---
+++ middlewares/games.js
@@ -0,0 +1,8 @@
+const games = require('../models/game');
+
+const findAllGames = async (req, res, next)=>{
+    req.gamesArray = await games.find({});
+    next();
+};
+
+module.exports = findAllGames;
