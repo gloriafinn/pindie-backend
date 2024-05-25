254582f12fdfabd779fca9a9fc5540782215c441
smiledariya <smiledariya@mail.ru>
2024-05-25 18:02:45 +0300

    Добавлена авторизация

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,12 @@ const apiRouter=require("express").Router();
 const usersRouter=require("./users");
 const gamesRouter=require("./games");
 const categoriesRouter=require("./categories");
+const authRouter=require("./auth");
+
 
 apiRouter.use("/api",usersRouter);
 apiRouter.use("/api",gamesRouter);
 apiRouter.use("/api",categoriesRouter);
+apiRouter.use("/api",authRouter);
 
 module.exports= apiRouter;
