d362a9bfba00ee932b38f01d32c6d904ed99f8d5
smiledariya <smiledariya@mail.ru>
2024-05-27 14:53:23 +0300

    Добавлено все

--- routes/page.js
+++ routes/page.js
@@ -2,10 +2,11 @@
 // Импорты и другие маршруты
 
 const pagesRouter = require("express").Router();
-const { sendIndex } = require("../controllers/auth.js");
+const { checkCookiesJWT, checkAuth } = require("../middlewares/auth.js");
+const { sendIndex, sendDashboard } = require("../controllers/auth.js");
+
 
 pagesRouter.get("/", sendIndex); 
-pagesRouter.get("/admin/**", sendDashboard); 
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
 
 module.exports=pagesRouter;
